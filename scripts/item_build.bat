593d50628e2b57901b787e2bdb630842f2a0af3e
TimurRakhimov <lollipopnote8@gmail.com>
2026-08-26 08:19:51 +0300

    feature: Migrate LETO_PATH from a CMake variable to a system environment

--- scripts/item_build.bat
+++ scripts/item_build.bat
@@ -1,6 +1,9 @@
 @echo off
-set "ROOT=%cd%"
+if "%LETO_PATH%"=="" (
+	echo [ERROR] Переменная окружения LETO_PATH не задана.
+	echo Задайте её один раз командой: setx LETO_PATH "C:/путь/к/папке/с/репозиториями"
+	exit /b 1
+)
 cd /d "%~1"
-cmake --preset %~2 -DLETO_PATH="%ROOT%"
+cmake --preset %~2
 cmake --build -j --preset %~2
-
