262a67db2373860593a6549fbad19fde2318ddd4
TimurRahimov <lollipopnote8@gmail.com>
2026-09-03 09:14:27 +0300

    fix: preset_setup and setup for Linux

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -6,5 +6,5 @@ echo "[LetoAPI] Собираем проект..."
     cd "$(dirname "${BASH_SOURCE}")" || exit 1
 
     #./preset_setup.sh LetoAPI ubuntu-debug
-    ./preset_setup.sh LetoAPI stm32f411xe-debug
+    ./preset_setup.sh LetoAPI stm32f411xe-st7735-debug
 )
